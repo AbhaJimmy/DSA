ccb2bcc6f43a51cd32dbeaea2c0a06fa78bdc482
Abha <asj27112@gmail.com>
2025-07-29 22:28:26 +0530

    Array Qs

--- Arrays/twoSum.cpp
+++ Arrays/twoSum.cpp
@@ -4,15 +4,15 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int,int> map;
-        int i, first, difference;
-        for (i=0; i<nums.size(); i++) {
-            first = nums[i];
-            difference = target-first;
-            if (map.find(difference) != map.end()) {
-                return {map[difference], i};
+        map<int,int> mpp;
+        int n=nums.size(), num, difference;
+        for (int i=0; i<n; i++) {
+            num = nums[i];
+            difference = target - num;
+            if (mpp.find(difference) != mpp.end()) {
+                return {mpp[difference], i};
             }
-            map[first] = i;
+            mpp[num] = i;
         }
         return {-1,-1};
     }
