e181de9557428c16d4d6ef55cd4d6e3652096545
Abha <asj27112@gmail.com>
2025-08-01 23:52:17 +0530

    Array Qs

---
+++ Arrays/maxSubarray.cpp
@@ -0,0 +1,15 @@
+// LeetCode - 53 - Maximum Subarray
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size(), sum=0, maxSum=INT_MIN;
+        for (int i=0; i<n; i++) {
+            sum += nums[i];
+            maxSum = max(maxSum,sum);
+            if (sum < 0) sum=0;
+        }
+        return maxSum;
+    }
+};
