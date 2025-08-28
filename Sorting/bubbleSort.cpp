d3f93ea5a3c8034165f5c7351b269181981ed46d
Abha <asj27112@gmail.com>
2025-08-29 00:02:55 +0530

    Sorting

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -4,14 +4,16 @@ using namespace std;
 class Solution {
   public:
     void bubbleSort(vector<int>& arr) {
-        // code here
+        bool didSwap = false;
         int n=arr.size();
         for (int i=n-1; i>=1; i--) {
             for (int j=0; j<i; j++) {
                 if (arr[j] > arr[j+1]) {
                     swap(arr[j],arr[j+1]);
+                    didSwap = true;
                 }
             }
+            if (!didSwap) break;
         }
     }
 };
