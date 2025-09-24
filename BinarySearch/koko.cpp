3a9769518e5c26134b1abeb91706776568ed825b
Abha <asj27112@gmail.com>
2025-09-25 00:07:36 +0530

    Binary Search

--- BinarySearch/koko.cpp
+++ BinarySearch/koko.cpp
@@ -3,11 +3,10 @@
 using namespace std;
 class Solution {
 public:
-    int totalHours(vector<int>& piles, int hours, int n) {
+    long long totalHours(vector<int>& piles, int hours, int n) {
         long long sum = 0;
         for (int i=0; i<n; i++) {
-            long long pile = piles[i];
-            sum += ceil((double)pile/(double)hours);
+            sum += ceil((double)piles[i]/(double)hours);
         }
         return sum;
     }
@@ -17,10 +16,10 @@ public:
         return maxi;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n=piles.size(), low=1, high=maxElement(piles,n), mid, hours;
+        int n=piles.size(), low=1, high=maxElement(piles,n), mid;
         while (low <= high) {
             mid = (low+high)/2;
-            hours = totalHours(piles,mid,n);
+            long long hours = totalHours(piles,mid,n);
             if (hours <= h) high = mid-1;
             else low = mid+1;
         }
