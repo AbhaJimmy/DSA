4ec7ef12ba40c63187d2286f54daa3d26ab4ca3f
Abha <asj27112@gmail.com>
2025-09-27 23:00:50 +0530

    Binary Search

---
+++ BinarySearch/kthElement.cpp
@@ -0,0 +1,9 @@
+// GFG - Kth Element of 2 sorted arrays
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int kthElement(vector<int> &a, vector<int> &b, int k) {
+    
+    }
+};
