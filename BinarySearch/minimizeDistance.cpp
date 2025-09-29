680b01395b4db80f42d38d853743a15c0cbc2f86
Abha <asj27112@gmail.com>
2025-09-29 23:25:53 +0530

    Binary Search

---
+++ BinarySearch/minimizeDistance.cpp
@@ -0,0 +1,8 @@
+// GFG - Minimize Max Distance to Gas Station
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    double minMaxDist(vector<int> &stations, int K) {
+    }
+};
