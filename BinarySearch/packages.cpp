53d046e477c8bdbdc50c68a9bed796c9590ee596
Abha <asj27112@gmail.com>
2025-10-03 23:20:53 +0530

    Binary Search

---
+++ BinarySearch/packages.cpp
@@ -0,0 +1,9 @@
+// LeetCode - 1011 - Capacity To Ship Packages Within D Days
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        
+    }
+};
