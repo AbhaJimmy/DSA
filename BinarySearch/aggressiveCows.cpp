0f9786c417021999d25c405f39699d779622b825
Abha <asj27112@gmail.com>
2025-10-08 23:31:09 +0530

    Binary Search

--- BinarySearch/aggressiveCows.cpp
+++ BinarySearch/aggressiveCows.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution {
   public:
     bool canWePlace(vector<int> &stalls, int dist, int cows) {
-        
+        int n=stalls.size(), count=1;
     }
     int aggressiveCows(vector<int> &stalls, int k) {
         int n=stalls.size();      
