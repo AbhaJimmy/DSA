45fdac739afa5a82d26643679fb5d06abbf7d70e
Abha <asj27112@gmail.com>
2025-10-09 23:43:08 +0530

    Binary Search

--- BinarySearch/aggressiveCows.cpp
+++ BinarySearch/aggressiveCows.cpp
@@ -7,7 +7,7 @@ class Solution {
         int n=stalls.size(), count=1;
     }
     int aggressiveCows(vector<int> &stalls, int k) {
-        int n=stalls.size();      
+        int n = stalls.size(), low = 1, high = stalls[n-1] - stalls[0];      
         sort(stalls.begin(),stalls.end());
     }
 };
