8870e45e952366a2bc402432dce93a4b73312afa
Abha <asj27112@gmail.com>
2025-10-16 23:46:13 +0530

    Binary Search

--- BinarySearch/aggressiveCows.cpp
+++ BinarySearch/aggressiveCows.cpp
@@ -6,7 +6,7 @@ class Solution {
     bool canWePlace(vector<int> &stalls, int dist, int cows) {
         int n=stalls.size(), count=1, last=stalls[0];
         for (int i=1; i<n; i++) {
-            return true;
+            if (count >= cows) return true;
         }
         return false;
     }
