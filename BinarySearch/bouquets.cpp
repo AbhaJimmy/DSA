c8732686dc7ca0a8af8a9a1d6cdecdc9778ed826
Abha <asj27112@gmail.com>
2025-10-21 23:37:30 +0530

    Binary Search

---
+++ BinarySearch/bouquets.cpp
@@ -0,0 +1,9 @@
+// LeetCode - 1482 - Minimum Number of Days to Make m Bouquets
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        
+    }
+};
