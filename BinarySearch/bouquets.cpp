ffd4ffddc23ca527ffa80a9a17afbfc6c55fa18f
Abha <asj27112@gmail.com>
2025-10-23 23:01:24 +0530

    Binary Search

--- BinarySearch/bouquets.cpp
+++ BinarySearch/bouquets.cpp
@@ -5,5 +5,7 @@ class Solution {
 public:
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n=bloomDay.size();
+        long long val = m * 1ll * k * 1ll;
+        if (val > n) return -1;
     }
 };
