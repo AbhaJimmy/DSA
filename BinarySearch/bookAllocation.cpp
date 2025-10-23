e05796dcdadae2d7f1d995994e2f1dddebf87ee8
Abha <asj27112@gmail.com>
2025-10-23 23:06:35 +0530

    Binary Search

---
+++ BinarySearch/bookAllocation.cpp
@@ -0,0 +1,10 @@
+// GFG - Allocate Minimum Pages
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int findPages(vector<int> &arr, int k) {
+        // code here
+        
+    }
+};
