0a1450072a6220db92bc2b8a0ced7d2c45aec5f9
Abha <asj27112@gmail.com>
2025-11-02 23:48:29 +0530

    Binary Search

--- BinarySearch/bookAllocation.cpp
+++ BinarySearch/bookAllocation.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 class Solution {
   public:
+    int countStudents(vector<int> &arr, int pages) {
+      
+    }
     int findPages(vector<int> &arr, int k) {
         int n=arr.size(), students=1;
         long long pagesStudent = 0;
