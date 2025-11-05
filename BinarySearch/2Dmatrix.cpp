e67e70913d70e916901b51e38ca1d6269a9afc68
Abha <asj27112@gmail.com>
2025-11-05 23:36:58 +0530

    Binary Search

---
+++ BinarySearch/2Dmatrix.cpp
@@ -0,0 +1,9 @@
+// LeetCode - 74 - Search a 2D Matrix
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+    }
+};
