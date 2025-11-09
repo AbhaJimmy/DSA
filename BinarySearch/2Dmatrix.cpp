6d110528861d2ea295afde9dfe0427e07812e4c4
Abha <asj27112@gmail.com>
2025-11-09 21:53:53 +0530

    Binary Search

--- BinarySearch/2Dmatrix.cpp
+++ BinarySearch/2Dmatrix.cpp
@@ -4,6 +4,6 @@ using namespace std;
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n=matrix.size(), m=matrix[0].size();
+        int n=matrix.size(), m=matrix[0].size(), low=0, high=n*m-1;
     }
 };
