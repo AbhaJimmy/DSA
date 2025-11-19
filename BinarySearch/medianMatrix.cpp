e8fd31fa944b5af158f25652efcb7a76f6e17782
Abha <asj27112@gmail.com>
2025-11-19 23:05:15 +0530

    Binary search

--- BinarySearch/medianMatrix.cpp
+++ BinarySearch/medianMatrix.cpp
@@ -5,6 +5,6 @@ class Solution {
   public:
     int median(vector<vector<int>> &mat) {
         int n=mat.size(), m=mat[0].size();
-        
+        int low = mat[0][0], high = mat[0][m-1];
     }
 };
